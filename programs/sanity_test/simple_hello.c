0a9b8b97c24d76ad6cfa3057da0b3d4301309aab
annachiara19 <ruospoannachiara@gmail.com>
2023-09-13 16:20:43 +0000

    added konata capabilities

--- programs/sanity_test/simple_hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-
-int main (void){
-
-    printf("Hello Gem5 from RISCV\n");
-    return 0;
-}
--- simulate.sh
+++ simulate.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-if [[ $# -ne 1 ]] ; then 
+# Command line parameters
+# 1. Program folder
+# 2. -gui/-nogui = launch Konata
+
+
+
+if [[ $# -ne 2 ]] ; then 
 echo "Wrong number of parameters"
 echo "Usage  $0 path_to_program_folder" 
 exit 1
@@ -11,6 +17,8 @@ fi
 export program_folder=$1
 export program=$(basename ${program_folder})
 echo "Simulating ${program}"
+
+export gui=$2
 
 if [[ ! -d "${RESULTS_DIR}/${program}" ]] ; then 
 mkdir ${RESULTS_DIR}/${program}
@@ -25,18 +33,25 @@ cd ${WORK_DIR}
 
 echo "Simulating program ${program}"
 
-gem5_options="--debug-flags=O3PipeView,O3CPUAll --debug-file=${RESULTS_DIR}/${program}/trace.out --outdir=${RESULTS_DIR}/${program}/ --verbose "
-##--stats-file=FILE       Sets the output file for statistics [Default:   stats.txt]
-# --dump-config=FILE      Dump configuration output file [Default: config.ini]
+gem5_options="--debug-flags=O3PipeView,O3CPUAll --debug-start=${TICKS_START} --debug-file=${RESULTS_DIR}/${program}/trace.out --outdir=${RESULTS_DIR}/${program}/ --verbose "
+
+# Debugging options
+# --stats-file=FILE            Sets the output file for statistics [Default:   stats.txt]
+# --dump-config=FILE           Dump configuration output file [Default: config.ini]
+# --debug-start=TIME           Start debug output at TIME (must be in ticks)             #goal: start debugging since main
+# --debug-break=TIME[,TIME]    Tick to create a breakpoint
+# -m : last cycle of interest 
+
 
 ### NOTE : --output=${RESULTS_DIR}/${program}/program.out REMOVE IF YOU WANNNA SEE STDOUT IN TERMINAL 
-simulation_script_option="--caches  -c ${program_folder}/${program}.elf --directory=${RESULTS_DIR}/${program}  --errout=${RESULTS_DIR}/${program}/programm.err "  
+simulation_script_option="--caches  -c ${program_folder}${program}.elf --directory=${RESULTS_DIR}/${program}  --errout=${RESULTS_DIR}/${program}/programm.err "  
 
 
 echo "${GEM5_INSTALLATION_PATH}/${GEM5_ISA}/gem5.${GEM5_VARIANT} ${gem5_options} ${GEM5_SIMULATION_SCRIPT} ${simulation_script_option}" 
 ${GEM5_INSTALLATION_PATH}/${GEM5_ISA}/gem5.${GEM5_VARIANT} ${gem5_options} ${GEM5_SIMULATION_SCRIPT} ${simulation_script_option}
 
-if ${NATIVE_LINUX}; then 
-## launch konata ./konata.sh
-${KONATA} ${RESULTS_DIR}/${program}/trace.out
-fi 
+# Launch Konata (TODO: add if option)
+if [[ ${gui} == "-gui" ]] ; then
+	${PATH_KONATA}konata  ${RESULTS_DIR}/${program}/trace.out &
+fi
+
