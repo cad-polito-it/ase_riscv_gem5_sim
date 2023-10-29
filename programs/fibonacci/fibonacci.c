a53c57ad9c823f9fbb8ce2e76eb6130c9ddba94c
franout <francesco.angione@polito.it>
2023-10-29 21:32:49 +0100

    bug fix for roi, added roi for all benchmarks

--- programs/fibonacci/fibonacci.c
+++ programs/fibonacci/fibonacci.c
@@ -1,5 +1,14 @@
+#ifdef _GEM5_
+#include <gem5/m5ops.h>
+#endif /*_GEM5_*/
+
 
 int main() {
+
+#if _GEM5_
+m5_work_begin(1,1);   
+#endif /*_GEM5_*/
+
     float n = 5.0, first = 0, second = 1, next;
 
     for (int i = 0; i < n; i++) {
@@ -11,6 +20,9 @@ int main() {
             second = next;
         }
     }
+#ifdef _GEM5_
+    m5_work_end(1,1);
+#endif /*_GEM5_*/
 
     return 0;
 }
