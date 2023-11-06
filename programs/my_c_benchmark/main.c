fada6112da6cf949b26d57dcfa509aab85f3dca5
franout <francesco.angione@polito.it>
2023-11-06 18:02:24 +0100

    fixed include in makefile

--- programs/my_c_benchmark/main.c
+++ programs/my_c_benchmark/main.c
@@ -51,6 +51,7 @@ int main() {
  *****      End Region of Interest (ROI)         ********
 ********************************************************/
 #ifdef _GEM5_
+    for(i=0; i<100; i++);
     m5_work_end(1,1);
 #endif /*_GEM5_*/
     return 0;
