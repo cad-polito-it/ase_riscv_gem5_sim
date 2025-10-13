8a031a917c0b40de4e0de6e92ec3d536de3b069e
francesco <francesco.angione@polito.it>
2025-10-13 18:48:08 +0200

    fix: typo and general fixes with improvements

--- utils/installation.sh
+++ utils/installation.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-ROOT_DIR=./tools
+WORK_DIR=$(pwd)
+ROOT_DIR="${WORK_DIR}/tools"
 
-GEM_5_REPOSITORY=https://github.com/cad-polito-it/gem5.git
-RISCV_TOOLCHAIN_REPOSITORY=https://github.com/riscv-collab/riscv-gnu-toolchain.git
-GEM5_VISUALIZER_REPOSITORY=https://github.com/cad-polito-it/gem5_visualizer.git
+GEM_5_REPOSITORY="https://github.com/cad-polito-it/gem5.git"
+RISCV_TOOLCHAIN_REPOSITORY="https://github.com/riscv-collab/riscv-gnu-toolchain.git"
+GEM5_VISUALIZER_REPOSITORY="https://github.com/cad-polito-it/gem5_visualizer.git"
 
 
-if [ -z "$QT_INSTALLATION_DIR" ]; then
+if [[ -z "${QT_INSTALLATION_DIR}" ]]; then
     echo "QT_INSTALLATION_DIR is not set. Please set it to the Qt installation directory."
     echo "You can download Qt from https://doc.qt.io/qt-6/get-and-install-qt.html"
     echo "And install the 6.8.3 version (VERY IMPORTANT)"
@@ -15,65 +16,71 @@ if [ -z "$QT_INSTALLATION_DIR" ]; then
     exit 1
 fi
 
-mkdir $ROOT_DIR
+if [[ -d "$ROOT_DIR" ]] ; then 
+    echo "WARNING: Tools directory already exists. Remove it if you want to reinstall"
+else 
+    mkdir $ROOT_DIR
+fi 
+
 
 cd $ROOT_DIR
 
-if [[ -d "$ROOT_DIR/gem5" ]] ; then 
-    echo "gem5 directory already exists. Remove it if you want to reinstall"
-    exit 1
+##############################################################################
+
+if [[ -d "${ROOT_DIR}/gem5/" ]]; then 
+    echo "gem5 directory already exists. Skipping Remove it if you want to reinstall"   
+else 
+    echo "Downloading gem5 tool"
+    git clone ${GEM_5_REPOSITORY}
+    cd gem5
+    scons build/RISCV/gem5.opt -j $(nproc)
+    cd ..
 fi
 
 ##############################################################################
 
-echo "Downloading gem5 tool"
-git clone $GEM_5_REPOSITORY
-cd gem5
-scons build/RISCV/gem5.opt -j $(nproc)
+if [[ -d "${ROOT_DIR}/riscv-toolchain/" ]] ; then 
+    echo "riscv-gnu-toolchain directory already exists. Skipping. Remove it if you want to reinstall"
+else
+    echo "Installing RISC-V toolchain"
+    git clone ${RISCV_TOOLCHAIN_REPOSITORY}
+    cd riscv-gnu-toolchain
+    ./configure --prefix=${ROOT_DIR}/riscv-toolchain  --enable-multilib
+    make -j $(nproc)   
+    make install 
 
-echo "Downloading RISC-V toolchain"
-cd ..
+    echo "Cleaning up"
+    rm -rf $ROOT_DIR/riscv-gnu-toolchain
+    cd ..
 
-if [[ -d "$ROOT_DIR/riscv-gnu-toolchain" ]] ; then 
-    echo "riscv-gnu-toolchain directory already exists. Remove it if you want to reinstall"
-    exit 1
 fi
-
 ##############################################################################
 
-echo "Installing RISC-V toolchain"
-git clone $RISCV_TOOLCHAIN_REPOSITORY
-cd riscv-gnu-toolchain
-git submodule update --init --recursive
-./configure --prefix=$ROOT_DIR/riscv-toolchain  --enable-multilib
-make -j $(nproc)   
-make install 
-
-echo "Cleaning up"
-rm -rf $ROOT_DIR/riscv-gnu-toolchain
-cd ..
-
-##############################################################################
-echo "Installing gem5 visualizer"
-git clone $GEM5_VISUALIZER_REPOSITORY
-cd gem5_visualizer
-mkdir build
-cd build
-export PATH="${QT_INSTALLATION_DIR}/libexec:${QT_INSTALLATION_DIR}/bin:$PATH"
-export QT_FRAMEWORK_BYPASS_LICENSE_CHECK=1
-export LIBGL_ALWAYS_SOFTWARE=1
-cmake .. -DCMAKE_BUILD_TYPE=Release -DQT_CMAKE_PREFIX_PATH=${QT_INSTALLATION_DIR}
-make -j $(nproc)
-mv gem5_pipeline_visualizer $ROOT_DIR/gem5_pipeline_visualizer
-cd ../..   
-echo "Cleaning up"
-rm -rf $ROOT_DIR/gem5_visualizer
+if [[ -f "${ROOT_DIR}/gem5_pipeline_visualizer" ]]; then 
+    echo "gem5_pipeline_visualizer already exists. Skipping. Remove it if you want to reinstall"
+else 
+    echo "Installing gem5 visualizer"
+    git clone ${GEM5_VISUALIZER_REPOSITORY}
+    cd gem5_visualizer
+    mkdir build
+    cd build
+    export PATH="${QT_INSTALLATION_DIR}/libexec:${QT_INSTALLATION_DIR}/bin:$PATH"
+    export QT_FRAMEWORK_BYPASS_LICENSE_CHECK=1
+    export LIBGL_ALWAYS_SOFTWARE=1
+    cmake .. -DCMAKE_BUILD_TYPE=Release -DQT_CMAKE_PREFIX_PATH=${QT_INSTALLATION_DIR}
+    make -j $(nproc)
+    mv gem5_pipeline_visualizer ${ROOT_DIR}/gem5_pipeline_visualizer
+    cd ../..   
+    echo "Cleaning up"
+    rm -rf ${ROOT_DIR}/gem5_visualizer
+fi
 
 ##############################################################################
 
 echo "Installation completed"
 echo "Updating setup_default with new paths"
 
+cd ${WORK_DIR}
 sed -i "s|^export CC=.*|export CC=$ROOT_DIR/riscv-toolchain/bin/riscv64-unknown-elf-gcc|" setup_default
 sed -i "s|^export OBJDUMP=.*|export OBJDUMP=$ROOT_DIR/riscv-toolchain/bin/riscv64-unknown-elf-objdump|" setup_default
 sed -i "s|^export CC_INSTALLATION_PATH=.*|export CC_INSTALLATION_PATH=$ROOT_DIR/riscv-toolchain/bin/|" setup_default
